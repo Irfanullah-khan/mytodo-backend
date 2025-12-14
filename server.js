22806574141c86171bd64f4db17aeae91c142860
Irfanullah khan <irfanullah137298@gmail.com>
2025-12-15 00:50:26 +0500

    Prepare backend for Render deployment

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+
+app.use(cors({
+  origin: "https://mytodoapplicata.netlify.app/",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Debug Middleware
@@ -42,3 +48,7 @@ mongoose.connect(MONGO_URI)
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
