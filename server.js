310322a204b9d9d9aff6d88455833b9cc9decc2f
Irfanullah khan <irfanullah137298@gmail.com>
2025-12-16 09:53:36 +0500

    fix cors

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 
+// app.use(cors({
+//   origin: "https://mytodoapplicata.netlify.app",
+//   credentials: true
+// }));
 app.use(cors({
-  origin: "https://mytodoapplicata.netlify.app/",
+  origin: true,
   credentials: true
 }));
 
